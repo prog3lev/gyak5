0071f5f1d0558015fad21084bb36abe3c22a8b93
proba1 <proba1@pmmf.hu>
2017-12-16 09:36:13 +0100

    gyak5 indulo projekt

---
+++ gyak5_kezdo/src/sql/diakok.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE DIAKOK ( id int not null primary key, 
+                              nev varchar(40), 
+                              kod varchar(40), 
+                              szuletesiev int,
+			      koltsegteriteses boolean);
+
+INSERT INTO DIAKOK VALUES(1,'Orosz Imre','ORIMABC.PTE',1995,false);
+INSERT INTO DIAKOK VALUES(2,'Varga Koppány','VAKOABC.PTE',1989,true);
+INSERT INTO DIAKOK VALUES(3,'Kun Ágota','KUAGABC.PTE',1994,false);
+INSERT INTO DIAKOK VALUES(4,'Szilágyi Dezső','SZDEABC.PTE',1992,false);
+INSERT INTO DIAKOK VALUES(5,'Balogh Béla','BABEABC.PTE',1991,true);
+INSERT INTO DIAKOK VALUES(6,'Sipos Zalán','SIZAABC.PTE',1994,false);
+INSERT INTO DIAKOK VALUES(7,'Faragó Bálint','FABAABC.PTE',1990,true);
+INSERT INTO DIAKOK VALUES(8,'Csordás Ibolya','CSIBABC.PTE',1992,true);
+INSERT INTO DIAKOK VALUES(9,'Fábián Éva','FAEVABC.PTE',1993,false);
+INSERT INTO DIAKOK VALUES(10,'Hegedűs András','HEANABC.PTE',1995,false);
---
+++ gyak5_kezdo/src/sql/tantargyak.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE TANTARGYAK ( id int not null primary key, 
+                              nev varchar(40),
+			      kod varchar(40),  
+                              kredit int);
+
+
+INSERT INTO TANTARGYAK VALUES(1,'Programozás1','PRG1',3);
+INSERT INTO TANTARGYAK VALUES(2,'Programozás2','PRG2',4);
+INSERT INTO TANTARGYAK VALUES(3,'Programozás3','PRG3',5);
+INSERT INTO TANTARGYAK VALUES(4,'Matematika','MAT',4);
+INSERT INTO TANTARGYAK VALUES(5,'Hálózatok1','HALO1',2);
+INSERT INTO TANTARGYAK VALUES(6,'Hálózatok2','HALO2',3);
+INSERT INTO TANTARGYAK VALUES(7,'Fizika','FIZ',2);
+
